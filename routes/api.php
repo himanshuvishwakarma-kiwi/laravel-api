0b4d4e9cfc1b0284b881437aa1e6ce12e57df46a
Himanshukiwi <himanshu.vishwakarma@kiwitech.com>
2022-06-28 12:12:14 +0530

    add routs for cache

--- routes/api.php
+++ routes/api.php
@@ -42,3 +42,26 @@ Route::group(['prefix' => 'posts','middleware' => ['jwt.verify']], function() {
     Route::post('remove-post/{postId}', [PostController::class, 'deletePost']);
 });
 
+ // Clear cache using reoptimized class
+Route::get('/optimize-clear', function() {
+    \Artisan::call('optimize:clear');
+    return 'optimize cache cleared';
+});
+
+//Clear route cache
+Route::get('/route-cache', function() {
+    \Artisan::call('route:cache');
+    return 'Routes cache cleared';
+});
+
+//Clear config cache
+Route::get('/config-cache', function() {
+    \Artisan::call('config:cache');
+    return 'Config cache cleared';
+}); 
+
+// Clear application cache
+Route::get('/clear-cache', function() {
+    \Artisan::call('cache:clear');
+    return 'Application cache cleared';
+});
