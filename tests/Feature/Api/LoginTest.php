41d628468e4ef7608c7156ec4c8a8474d395b1f9
Himanshukiwi <himanshu.vishwakarma@kiwitech.com>
2022-07-05 22:45:27 +0530

    add test case for login,email not register and update test cases

--- tests/Feature/Api/LoginTest.php
+++ tests/Feature/Api/LoginTest.php
@@ -4,6 +4,7 @@ namespace Tests\Feature;
 
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
+use Symfony\Component\HttpFoundation\Response;
 use Tests\TestCase;
 
 class LoginTest extends TestCase
@@ -14,7 +15,7 @@ class LoginTest extends TestCase
      * @return void
      */
     /** @test */
-    public function login_required_validation_errors()
+    public function test_login_required_validation_errors()
     {
         $data = [];
 
@@ -30,7 +31,7 @@ class LoginTest extends TestCase
             ]);
     }
 
-    public function login_invalid_error()
+    public function test_login_invalid_error()
     {
         $data = [
             'email'=>'John.Mathew@yopmail.com',
@@ -39,14 +40,14 @@ class LoginTest extends TestCase
 
         $response = $this->postJson('/api/login', $data);
 
-        $response->assertStatus(400)
+        $response->assertStatus(Response::HTTP_BAD_REQUEST)
             ->assertJson([
                 'success'=>false,
                 'message' =>"Login credentials are invalid."
             ]);
     }
 
-    public function login_successful()
+    public function test_login_successful()
     {
         $data = [
             'email'=>'John.Mathew@yopmail.com',
@@ -56,10 +57,23 @@ class LoginTest extends TestCase
         $response = $this->postJson('/api/login', $data);
 
         $response->assertStatus(200)
-            ->assertJson([
+            ->assertJsonStructure([
                 'success',
                 'token'
             ]);
     }
-
+    
+    public function test_returns_a_email_not_found_error_when_trying_to_login_with_non_register_email()
+    {
+        $data = [
+            'email'=>'example@yopmail.com',
+            'password'=>'123456789'
+        ];
+        $response = $this->postJson('/api/login', $data);
+        $response->assertStatus(Response::HTTP_NOT_FOUND)
+            ->assertJson([
+            'success'=>false,
+            'message' =>"Email doesn't found in our database."
+        ]);
+    }
 }
